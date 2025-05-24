79b8b6f76cf3235063cb95833df0e5d2978f4071
Wongirengjembuten <juzo.chn@gmail.com>
2025-05-25 01:49:27 +0700

    Add files via upload

---
+++ gamesdesktop/slot/jl.php
@@ -0,0 +1,87 @@
+<div class="content-area">
+                                    <div id="content" class="content-box">
+                                        
+                                        	
+	<style>
+	.panelbg h2
+	{
+		color:#000;	
+	}
+	</style>
+        <div class="panel panelbg">
+       <div class="desktop-only"><h3 class="pb-4">Slot</h3></div>
+           <div class="panel-body">
+           
+            <div style="margin-top: 10px">
+            
+            <style>
+    
+    .slotarea {
+        height: 800px;
+         overflow:auto;
+        width: 100%; 
+        display: grid; 
+        grid-template-columns: repeat(5, 1fr);
+        gap-y: 25px;
+        margin-bottom: 40px;
+    }
+    
+    .casinoarea {
+        overflow:auto;
+        width: 100%; 
+        display: grid; 
+        grid-template-columns: repeat(4, 1fr);
+        gap-y: 25px;
+        margin-bottom: 24px;
+    }
+    
+    .five {
+        grid-template-columns: repeat(5, 1fr);
+    }
+    
+    .gameitem {
+        padding: 20px;
+        padding-top: 0px;
+    }
+    
+    .gameitem>img {
+        width: 100%;
+        max-width: 100%;
+        object-fit: contain;
+    }
+    
+    .gameitem>figcaption {
+        overflow-wrap: break-word;
+    }
+    
+    .casinoimg {
+        border-radius: 10px;
+        border: 1px solid #000; 
+        box-shadow:rgba(0,0,0,.1) 0px 2px 5px 5px;
+    }
+    
+    @media only screen and (max-width: 768px) {
+        .slotarea, .casinoarea {
+            height: 100%;
+            grid-template-columns: repeat(3, 1fr)!important;
+        }
+        
+        .gameitem {
+            padding: 6px;
+        }
+        
+        
+    }
+    
+</style>
+
+
+<h2 style="margin-bottom:30px;"><img src="https://img.pay4d.info/jili.png" alt="" style="margin-top:-20px">JILI</h2><div class="slotarea"><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/223.png"><figcaption style="text-align:center; margin-top:5px">Fortune Gems 2</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/145.png"><figcaption style="text-align:center; margin-top:5px">Neko Fortune</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/103.png"><figcaption style="text-align:center; margin-top:5px">Golden Empire</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/30.png"><figcaption style="text-align:center; margin-top:5px">Bubble Beauty</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/109.png"><figcaption style="text-align:center; margin-top:5px">Fortune Gems</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/49.png"><figcaption style="text-align:center; margin-top:5px">Super Ace</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/35.png"><figcaption style="text-align:center; margin-top:5px">Crazy 777</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/102.png"><figcaption style="text-align:center; margin-top:5px">Roma X</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/134.png"><figcaption style="text-align:center; margin-top:5px">Mega Ace</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/92.png"><figcaption style="text-align:center; margin-top:5px">Crazy Hunter</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/16.png"><figcaption style="text-align:center; margin-top:5px">Jungle King</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/77.png"><figcaption style="text-align:center; margin-top:5px">Boxing King</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/47.png"><figcaption style="text-align:center; margin-top:5px">Charge Buffalo</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/110.png"><figcaption style="text-align:center; margin-top:5px">Ali Baba</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/78.png"><figcaption style="text-align:center; margin-top:5px">Secret Treasure</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/144.png"><figcaption style="text-align:center; margin-top:5px">JILI Caishen</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/137.png"><figcaption style="text-align:center; margin-top:5px">Gold Rush</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/85.png"><figcaption style="text-align:center; margin-top:5px">Pharaoh Treasure</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/27.png"><figcaption style="text-align:center; margin-top:5px">Seven Seven Seven</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/38.png"><figcaption style="text-align:center; margin-top:5px">Fengshen</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/45.png"><figcaption style="text-align:center; margin-top:5px">Golden Bank</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/130.png"><figcaption style="text-align:center; margin-top:5px">Thor X</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/21.png"><figcaption style="text-align:center; margin-top:5px">Fa Fa Fa</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/183.png"><figcaption style="text-align:center; margin-top:5px">Golden Joker</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/181.png"><figcaption style="text-align:center; margin-top:5px">Wild Ace</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/176.png"><figcaption style="text-align:center; margin-top:5px">Master Tiger</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/164.png"><figcaption style="text-align:center; margin-top:5px">Pirate Queen</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/166.png"><figcaption style="text-align:center; margin-top:5px">Wild Racer</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/153.png"><figcaption style="text-align:center; margin-top:5px">Crazy Pusher</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/146.png"><figcaption style="text-align:center; margin-top:5px">World Cup</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/142.png"><figcaption style="text-align:center; margin-top:5px">Bonus Hunter</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/136.png"><figcaption style="text-align:center; margin-top:5px">Samba</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/135.png"><figcaption style="text-align:center; margin-top:5px">Mayan Empire</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/126.png"><figcaption style="text-align:center; margin-top:5px">Bones Fortune</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/116.png"><figcaption style="text-align:center; margin-top:5px">Happy Taxi</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/115.png"><figcaption style="text-align:center; margin-top:5px">Agent Ace</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/108.png"><figcaption style="text-align:center; margin-top:5px">Magic Lamp</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/106.png"><figcaption style="text-align:center; margin-top:5px">Twin Wins</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/101.png"><figcaption style="text-align:center; margin-top:5px">Medusa</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/100.png"><figcaption style="text-align:center; margin-top:5px">Super Rich</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/91.png"><figcaption style="text-align:center; margin-top:5px">Lucky Coming</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/89.png"><figcaption style="text-align:center; margin-top:5px">Lucky Lady</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/87.png"><figcaption style="text-align:center; margin-top:5px">Book of Gold</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/76.png"><figcaption style="text-align:center; margin-top:5px">Party Night</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/69.png"><figcaption style="text-align:center; margin-top:5px">Jump Sheep</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/67.png"><figcaption style="text-align:center; margin-top:5px">Monkey Party</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/66.png"><figcaption style="text-align:center; margin-top:5px">Lucky Number</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/64.png"><figcaption style="text-align:center; margin-top:5px">Fairness Games</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/63.png"><figcaption style="text-align:center; margin-top:5px">Seven Up Down</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/62.png"><figcaption style="text-align:center; margin-top:5px">Dice</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/61.png"><figcaption style="text-align:center; margin-top:5px">DragonTiger</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/58.png"><figcaption style="text-align:center; margin-top:5px">Golden Queen</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/48.png"><figcaption style="text-align:center; margin-top:5px">Lucky Goldbricks</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/46.png"><figcaption style="text-align:center; margin-top:5px">Dragon Treasure</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/44.png"><figcaption style="text-align:center; margin-top:5px">Diamond Party</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/43.png"><figcaption style="text-align:center; margin-top:5px">Xi Yang Yang</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/40.png"><figcaption style="text-align:center; margin-top:5px">Crazy FaFaFa</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/37.png"><figcaption style="text-align:center; margin-top:5px">Night City</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/36.png"><figcaption style="text-align:center; margin-top:5px">Bao boon chin</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/33.png"><figcaption style="text-align:center; margin-top:5px">Fortune Pig</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/26.png"><figcaption style="text-align:center; margin-top:5px">Hawaii Beauty</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/23.png"><figcaption style="text-align:center; margin-top:5px">Candy Baby</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/18.png"><figcaption style="text-align:center; margin-top:5px">Rock Beauty</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/17.png"><figcaption style="text-align:center; margin-top:5px">Shanghai Beauty</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/14.png"><figcaption style="text-align:center; margin-top:5px">Hyper Burst</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/13.png"><figcaption style="text-align:center; margin-top:5px">Lucky Ball</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/10.png"><figcaption style="text-align:center; margin-top:5px">Gem Party</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/9.png"><figcaption style="text-align:center; margin-top:5px">War Of Dragons</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/6.png"><figcaption style="text-align:center; margin-top:5px">Fortune Tree</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/5.png"><figcaption style="text-align:center; margin-top:5px">Hot Chilli</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/4.png"><figcaption style="text-align:center; margin-top:5px">God Of Martial</figcaption></figure><figure class="gameitem"><img src="https://img.pay4d.info/jl/images/2.png"><figcaption style="text-align:center; margin-top:5px">Chin Shi Huang</figcaption></figure></div><div style="clear:both"></div>            
+            
+            
+      		</div>
+    	</div>
+    </div>
+                                            
+                                    </div>
+                                </div>
