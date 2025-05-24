e7549d4e24c56cc9ea00a9c9538c30b35c80529b
Wongirengjembuten <juzo.chn@gmail.com>
2025-05-25 01:51:10 +0700

    Add files via upload

---
+++ gamesdesktop/sport/sport.php
@@ -0,0 +1,78 @@
+<div class="content-area">
+                                    <div id="content" class="content-box">
+                                        
+                                            <div class="panel panelbg">
+       <div class="desktop-only"><h3 class="pb-4">Sport</h3></div>
+           <div class="panel-body d-flex gap-4">
+           
+                <style>
+    
+    .slotarea {
+        height: 800px;
+         overflow:auto;
+        width: 100%; 
+        display: grid; 
+        grid-template-columns: repeat(5, 1fr);
+        gap-y: 25px;
+        margin-bottom: 40px;
+    }
+    
+    .casinoarea {
+        overflow:auto;
+        width: 100%; 
+        display: grid; 
+        grid-template-columns: repeat(4, 1fr);
+        gap-y: 25px;
+        margin-bottom: 24px;
+    }
+    
+    .five {
+        grid-template-columns: repeat(5, 1fr);
+    }
+    
+    .gameitem {
+        padding: 20px;
+        padding-top: 0px;
+    }
+    
+    .gameitem>img {
+        width: 100%;
+        max-width: 100%;
+        object-fit: contain;
+    }
+    
+    .gameitem>figcaption {
+        overflow-wrap: break-word;
+    }
+    
+    .casinoimg {
+        border-radius: 10px;
+        border: 1px solid #000; 
+        box-shadow:rgba(0,0,0,.1) 0px 2px 5px 5px;
+    }
+    
+    @media only screen and (max-width: 768px) {
+        .slotarea, .casinoarea {
+            height: 100%;
+            grid-template-columns: repeat(3, 1fr)!important;
+        }
+        
+        .gameitem {
+            padding: 6px;
+        }
+        
+        
+    }
+    
+</style>
+
+
+<figure style="text-align: center"><img src="https://img.pay4d.info/sabalogo.png" alt="Saba Sports"><figcaption>Saba Sports</figcaption></figure><figure style="text-align: center"><img src="https://img.pay4d.info/sbologo.png" alt="SBO Sports"><figcaption>SBO Sports</figcaption></figure><figure style="text-align: center"><img src="https://img.pay4d.info/tflogo.png" alt="ThunderFire E-Sports"><figcaption>ThunderFire E-Sports</figcaption></figure>
+            
+
+
+    	</div>
+    </div>
+                                            
+                                    </div>
+                                </div>
